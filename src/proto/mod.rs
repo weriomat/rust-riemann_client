4e10ac6e45927408af7100bdc03819d84bc84d11
weriomat <engel@weriomat.com>
2026-06-21 16:47:24 +0200

    Move the tests for proto into proto

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -5,3 +5,18 @@ pub mod mod_pb {
     include!(concat!(env!("OUT_DIR"), "/generated_with_pure/mod.rs"));
 }
 pub use self::mod_pb::riemann::*;
+
+#[test]
+fn query_from_str() {
+    let _ = Query::from("hello world");
+}
+
+#[test]
+fn query_from_string() {
+    let _ = Query::from("hello world".to_string());
+}
+
+#[test]
+fn query_from_query() {
+    let _ = Query::from(Query::new());
+}
